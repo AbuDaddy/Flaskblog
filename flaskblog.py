0084d6114b138eb5f16eb23298478ab3d1cea87e
AbuDaddy <abbamba06@gmail.com>
2023-10-14 03:07:41 +0200

    Templates - Blog

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [{
+    'title': 'Blog Post 1',
+    'author': 'Abou Bamba',
+    'date_posted': 'April 23, 2023',
+    'content': 'First Blog Post'
+}, {
+    'title': 'Blog Post 2',
+    'author': 'Jane Doe',
+    'date_posted': 'June 9, 2023',
+    'content': 'Second Blog Post'
+}]
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
